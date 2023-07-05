dcae3c00a3fb8e570f8bc96b4a1a2d402b4e66a0
mgfahem <mgfahem@gmail.com>
2023-07-05 19:11:55 +0200

    update: select a country

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ let newValueElements = document.querySelectorAll(".newValue");
 let totalPrice = 0;
 let itemPrices = document.querySelectorAll(".item-price p");
 
+let toggle = document.querySelector('#cities');
+let currentLi = document.querySelector("#currentLi");
+let newLi = document.querySelector(".newLi");
+
+// Form functions
+
+toggle.onclick = dropdownMenu;
+document.querySelector("#li1").addEventListener("click", selectACountry);
+document.querySelector("#li2").addEventListener("click", selectACountry); 
+document.querySelector("#li3").addEventListener("click", selectACountry); 
+document.querySelector("#li4").addEventListener("click", selectACountry); 
+document.querySelector("#li5").addEventListener("click", selectACountry); 
+
+function dropdownMenu() {
+  toggle.classList.toggle("opened");
+}
+
+function selectACountry(event) {
+  if (event.target.nodeName === 'LI') {
+    const value = event.target.textContent;
+    newLi.innerText = value;
+    currentLi.style.visibility = 'hidden';
+  }
+}
+
+// Cart functions
+
 addButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
     addItemToCart(index);
